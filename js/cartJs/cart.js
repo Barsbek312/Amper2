9c63c6a405fae62c7cc12b2133d483a08c399c55
Barsbek312 <98591566+Barsbek312@users.noreply.github.com>
2022-11-24 16:00:50 +0600

    remove clearStorage

--- js/cartJs/cart.js
+++ js/cartJs/cart.js
@@ -1,7 +1,5 @@
 'use strict';
 
-localStorage.clear()
-
 const topicsCardTemplate = document.querySelector('[data-topics-template]');
 const topicsCardContainer = document.querySelector('[data-topics-card-container]');
 
